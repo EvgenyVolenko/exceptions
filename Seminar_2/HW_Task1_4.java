9dc549a81ce995e42de6b212a50863faedeeacc8
Evgeny <evolenko@inbox.ru>
2023-08-03 23:31:16 +0300

    task1_4 done

--- Seminar_2/HW_Task1_4.java
+++ Seminar_2/HW_Task1_4.java
@@ -10,8 +10,28 @@ import java.util.Scanner;
 public class HW_Task1_4 {
     public static void main(String[] args) {
 
+        System.out.println("Вы ввели - " + getFloat());
+    }
+
+    public static float getFloat() {
         Scanner scanner = new Scanner(System.in);
-        float digit = scanner.nextFloat();
-        System.out.println(digit);
+        System.out.print("Введите дробное число: ");
+        String digitString;
+        float digit;
+
+        while (true) {
+            digitString = scanner.nextLine();
+            try {
+                digitString.charAt(0);
+                digit = Float.parseFloat(digitString);
+                break;
+            } catch (StringIndexOutOfBoundsException e) {
+                System.out.print("Пустые строки вводить нельзя. Повторите ввод: ");
+            } catch (NumberFormatException e) {
+                System.out.print("Вы ввели не число. Повторите ввод: ");
+            }
+        }
+        scanner.close();
+        return digit;
     }
 }
